76943795b8bca4c55a0b213d42ef4b1cb6eb65e5
x0y14 <ya01ma08@gmail.com>
2022-10-09 21:50:13 +0900

    移動バーとコンテンツを画面いっぱいに表示するように変更

--- components/slide.tsx
+++ components/slide.tsx
@@ -1,29 +1,32 @@
-import { useAtom } from 'jotai'
-import { atomWithImmer } from 'jotai/immer'
+import {useAtom} from 'jotai'
+import {atomWithImmer} from 'jotai/immer'
+import ArrowLeftIcon from '@mui/icons-material/ArrowLeft';
+import ArrowRightIcon from '@mui/icons-material/ArrowRight';
 
 const countAtom = atomWithImmer(0)
 
 interface PlayerProps {
     totalPageCount: number;
 }
+
 export const SlidePlayer = (props: PlayerProps) => {
     const [, setCount] = useAtom(countAtom)
     const decrement = () => setCount((c) => (
-        (0 < c)
-            ? c = c-1
-            : c
+            (0 < c)
+                ? c = c - 1
+                : c
         )
     )
     const increment = () => setCount((c) => (
-        (c < props.totalPageCount-1)
-            ? c = c+1
-            : c
+            (c < props.totalPageCount - 1)
+                ? c = c + 1
+                : c
         )
     )
     return (
-        <div>
-            <button onClick={decrement}>&larr;</button>
-            <button onClick={increment}>&rarr;</button>
+        <div className={"w-full bg-blue-800 flex justify-center"}>
+            <ArrowLeftIcon  onClick={decrement} className={"fill-white"} fontSize={"large"}>&larr;</ArrowLeftIcon>
+            <ArrowRightIcon onClick={increment} className={"fill-white"} fontSize={"large"}>&rarr;</ArrowRightIcon>
         </div>
     )
 }
@@ -31,11 +34,14 @@ export const SlidePlayer = (props: PlayerProps) => {
 interface PreviewProps {
     pages: string[]
 }
+
 export function SlidePreview(props: PreviewProps) {
-    const [currentPageNo, ] = useAtom(countAtom)
+    const [currentPageNo,] = useAtom(countAtom)
     return (
-        <div>
-        <div>slide page: {props.pages[currentPageNo]}</div>
+        <div className={"h-full w-full flex-1 flex border-2 justify-center items-center"}>
+            <div className={"flex bg-amber-100"}>
+                <div className={"text-2xl"}>page: {props.pages[currentPageNo]}</div>
+            </div>
         </div>
     )
 }
